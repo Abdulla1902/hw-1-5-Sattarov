3ef50dec752dbc8b3f57c7528fee936abdbd7a18
satta <sattarovabdulla@gmail.com>
2022-11-10 17:47:56 +0600

    Abdulla-hw

---
+++ main_app/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render, Http404, redirect
+from datetime import datetime
+from .models import Film, Director
+from .forms import FilmForm, DirectorForm, UserCreateForm
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def register_view(request):
+    context = {
+        'directors': Director.objects.all(),
+        'form': UserCreateForm()
+    }
+    if request.method == "POST":
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            User.objects.create_user(username, password=password)
+            return redirect('/login/')
+        context['form'] = form
+    return render(request, 'register.html', context)
+
+
+def create_director_view(request):
+    context = {
+        'directors': Director.objects.all(),
+        'form': DirectorForm()
+    }
+    if request.method == 'POST':
+        form = DirectorForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/films/')
+    return render(request, 'create_director.html', context)
+
+
+def create_film_view(request):
+    context = {
+        'directors': Director.objects.all(),
+        'form': FilmForm()
+    }
+    if request.method == 'POST':
+        form = FilmForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/films/')
+    return render(request, 'create_film.html', context)
+
+
+def index_view(request):
+    context = {
+        'directors': Director.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
+
+def about_us_view(request):
+    return render(request, 'about.html', context={'year': str(datetime.now().year - 2005)})
+
+
+def date_now_view(request):
+    cur_date = datetime.now()
+    context = {
+        'year': cur_date.year,
+        'month': cur_date.month,
+        'day': cur_date.day,
+        'hour': cur_date.hour,
+        'minute': cur_date.minute,
+    }
+
+    return render(request, 'date_now.html', context=context)
+
+
+def films_view(request):
+    context = {
+        'film_list': Film.objects.all(),
+        'directors': Director.objects.all(),
+    }
+
+    return render(request, 'films.html', context=context)
+
+
+def films_detail_view(request, id):
+    context = {
+        'film_detail': Film.objects.get(id=id),
+        'directors': Director.objects.all(),
+    }
+    return render(request, 'film_detail.html', context=context)
+
+
+def director_films_view(request, director_id):
+    try:
+        director = Director.objects.get(id=director_id)
+    except Director.DoesNotExist:
+        raise Http404
+    context = {
+        'director': director,
+        'directors': Director.objects.all()
+    }
+
+    return render(request, 'director_products.html', context=context)
